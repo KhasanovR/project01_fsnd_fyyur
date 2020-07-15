ea9927c0254e1f2fde21afeb164692cd0059d3bf
Rakhmatjon Khasanov <rakhmatjon.khasanov2711@gmail.com>
2020-07-15 12:02:29 +0500

    Connect the app to PostgreSQL database

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,4 +14,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_ENV')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
